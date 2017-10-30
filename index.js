c00f446df9e07862c2f1d3b1e9b06c38e674cdbb
Tim Shipman <bricklayertim@yahoo.com>
2017-10-30 17:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  name === you {
-    return ("Happy holidays, ${name}!")
+  "name === you"; {
+    return ("Happy holidays, ${name}!");
   }
 }
